764eb776215360ccbef86f188e4820dafb354d86
Lv9 <q153996072@gmail.com>
2013-02-01 09:46:04 +0800

    修正错误的包引用

--- example/pythonexample/bookexample/lession_1/lession_1_16.py
+++ example/pythonexample/bookexample/lession_1/lession_1_16.py
@@ -5,9 +5,9 @@ Created on 2011-12-2
 
 @author: Lv9
 '''
-import div #通过import创建一个新的命名空间,并在该命名空间中执行与.py文件相关的所有语句
-import div as d #可以通过as 起别名
-from div import divide;#也可以将具体的定义导入到当前的命名空间中(用from)
+import bookexample.lession_1.div as div#通过import创建一个新的命名空间,并在该命名空间中执行与.py文件相关的所有语句
+import bookexample.lession_1.div as d #可以通过as 起别名
+from bookexample.lession_1.div import divide;#也可以将具体的定义导入到当前的命名空间中(用from)
 # from div import *;#要把素有定义导入到当前的命名空间中也可以这样
 
 print(dir(div));#可以用dir扫描加载进来的命名空间
