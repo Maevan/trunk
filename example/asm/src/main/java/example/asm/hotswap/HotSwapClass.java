d4bff9d975bf1eb7092864c423e2e4bd9ec02411
Lv9 <q153996072@gmail.com>
2014-03-13 15:27:07 +0800

    提交ASM使用例子

---
+++ example/asm/src/main/java/example/asm/hotswap/HotSwapClass.java
@@ -0,0 +1,11 @@
+package example.asm.hotswap;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class HotSwapClass {
+	public static void main(String[] args) {
+
+	}
+}
