113477aa3e2d4fed4c25a19563df5497a3929106
Lv9 <q153996072@gmail.com>
2014-03-25 16:52:54 +0800

    测试提交服务是否好使

--- example/spring-boot/src/main/java/com/example/springboot/example/controller/ModelValueController.java
+++ example/spring-boot/src/main/java/com/example/springboot/example/controller/ModelValueController.java
@@ -1,7 +1,6 @@
 package com.example.springboot.example.controller;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
